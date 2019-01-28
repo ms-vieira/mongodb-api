f3f7465de7e59267f6b669f4604f0051f6c6798a
Matheus Silva <matheusv.silva@outlook.com>
2019-01-28 15:14:15 -0200

    Projeto Criado.

---
+++ src/main/java/com/app/mongodb/AppMongodbPersistenceApplication.java
@@ -0,0 +1,14 @@
+package com.app.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMongodbPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMongodbPersistenceApplication.class, args);
+	}
+
+}
+
