0a0abb94e0ef20e3b77ff394b68e9aca9cdb56db
Matheus Silva <matheusv.silva@outlook.com>
2019-02-02 13:09:17 -0200

    Implementando o authorDTO.

--- src/main/java/com/app/mongodb/config/Instantiation.java
+++ src/main/java/com/app/mongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.app.mongodb.config;
 
 import com.app.mongodb.domain.Post;
 import com.app.mongodb.domain.User;
+import com.app.mongodb.dto.AuthorDTO;
 import com.app.mongodb.repository.PostRepository;
 import com.app.mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,8 +39,8 @@ public class Instantiation implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null,sdf.parse("01/01/2019"), "Partiu mandela", "Vou viajar para o ABC. Abraços!", maria);
+        Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null,sdf.parse("01/01/2019"), "Partiu mandela", "Vou viajar para o ABC. Abraços!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/app/mongodb/domain/Post.java
+++ src/main/java/com/app/mongodb/domain/Post.java
@@ -1,5 +1,6 @@
 package com.app.mongodb.domain;
 
+import com.app.mongodb.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -16,10 +17,10 @@ public class Post  implements Serializable {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -63,11 +64,11 @@ public class Post  implements Serializable {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
