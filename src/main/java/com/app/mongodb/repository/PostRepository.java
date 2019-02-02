e465215781064c36fdd78b1f898fde641e6db772
Matheus Silva <matheusv.silva@outlook.com>
2019-02-02 14:55:09 -0200

    Finalizado.

--- src/main/java/com/app/mongodb/repository/PostRepository.java
+++ src/main/java/com/app/mongodb/repository/PostRepository.java
@@ -4,6 +4,8 @@ import com.app.mongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /*
     Utilizada para persistir no banco de dados.
 */
@@ -11,5 +13,6 @@ import org.springframework.stereotype.Repository;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 
+    List<Post> findByTitleContaining(String text);
 
 }
